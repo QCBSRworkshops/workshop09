a3d0f4ed419c907bba2a12fb82a6ff7fcbe05807
pedrohbraga <pedrohenrique.pereirabraga@mail.concordia.ca>
2021-07-28 20:38:07 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop09@953b576fd25a80cfbbeeefdc74e67ce70c9f70dd 🚀

--- book-en/01-preparing-for-the-workshop.R
+++
@@ -1,9 +0,0 @@
-## install.packages("vegan")
-## install.packages("gclus")
-## install.packages("ape")
-## 
-## library(vegan)
-## library(gclus)
-## library(ape)
-## 
-## source(file.choose()) # use coldiss.R which you have downloaded to your own directory
