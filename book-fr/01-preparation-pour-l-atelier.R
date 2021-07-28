9176acd764a8663ef1e7c194242a44e87b81d603
pedrohbraga <pedrohenrique.pereirabraga@mail.concordia.ca>
2021-07-28 20:38:36 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop09@2023c85fa62c2650533455343f11d19a2a548ff6 🚀

--- book-fr/01-preparation-pour-l-atelier.R
+++
@@ -1,9 +0,0 @@
-## install.packages("vegan")
-## install.packages("gclus")
-## install.packages("ape")
-## 
-## library(vegan)
-## library(gclus)
-## library(ape)
-## 
-## source(file.choose()) #coldiss.R
